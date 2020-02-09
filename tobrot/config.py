a00c15087ad7c48ed3bc4d399220382764295754
jazz199977 <33994071+jazz199977@users.noreply.github.com>
2020-02-09 18:58:25 +0530

    New
    None

---
+++ tobrot/config.py
@@ -0,0 +1,7 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = "931537265:AAEcaq1JcnFOJptAOCrZB7wCel8v0Ma-IsY"
+  APP_ID = 1007521
+  API_HASH = "224902e2e72677a86e4c64767d1c8db5"
+  AUTH_CHANNEL = -1001436278883
